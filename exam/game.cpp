adfb81f06a486d987ae7aedcad8f692dee9abf8f
kami <jingzihao.53@gmail.com>
2016-10-13 22:11:55 -0700

    game pass all test

--- exam/game.cpp
+++ exam/game.cpp
@@ -5,7 +5,7 @@ int A[10] = {};
 	//helper
 	bool Is_DuiTang(int size, int arr[]){
 
-		int k[1000] ={};
+		int k[1000] = {};
 		for(int i = 1 ; i<=6;i++ ){
 			if(k[arr[i]] == 0){
 				k[arr[i]] = 1;
@@ -28,28 +28,28 @@ int A[10] = {};
 		return count;
 	}
 	
-	//某个点数出现恰好四次，且不是状元
-	// how to use sort to do this ? 
+	// 某个点数出现恰好四次，且不是状元
+    // how to use sort to do this ? 
 	int fourOfX(int arr[]){
-		int k[1000];
-		fill_n(k, 1000, 1);
-		for(int i = 1;i<= 6;i++){
+		int k[1000] ={};
+		//fill_n(k, 1000, 0);
+		for(int i = 1; i<= 6; i++){
+			k[arr[i]] = k[arr[i]] + 1;	
+		}
+		//cout << k[2] << "jin two ju"<<endl;
+		for(int i = 1; i<= 6; i++){
 			if(k[arr[i]] == 4){
 				return 1;
 			}
-			k[arr[i]] = k[arr[i]] + 1;	
 		}
-		return 0;		
+		return 0;	
 	}
 		
 
 int main(){
 	int total = 0;
-	cin >> total;
-	
-	
-	while(total > 0){
-		total--;
+	cin >> total;	
+	for(int i = total; i >0; i--){
 		//read next t
 		for(int i =1; i <=6; i++){
 			cin >> A[i];
@@ -57,28 +57,29 @@ int main(){
 		//cout << "1"<<endl;
 		if(Is_DuiTang(6, A)){
 		//cout << "2"<<endl;
-			cout << "對堂"<<endl;
+			cout << "對堂" << endl;
 		}else{
 		//cout << "3"<<endl;	
 			if(numberOfX(4, A) == 4){
 				if(numberOfX(1,A) == 2){
-					cout <<"狀元插金花"<<endl;				
-				}else{
-					cout <<"狀元"<<endl;
+					cout << "狀元插金花"<< endl;				
 				}
+				cout << "狀元"<< endl;
+				continue;
 			}else if(numberOfX(4,A) == 3){
 				cout <<"三紅"<<endl;
 				continue;		
 			}else if(fourOfX(A)){
-				//cout << " inside"<<endl;
 					if(numberOfX(4, A) == 2){
-						cout << "四進帶二舉"<<endl;
+						cout <<"四進帶二舉"<<endl;
 						continue;
 					}
 					if(numberOfX(4, A) == 1){
-						cout << "四進帶一秀"<<endl;
+						cout <<"四進帶一秀"<<endl;
 						continue;
-					}	
+					}
+					cout <<"四進"<<endl;
+					continue;	
 			}else if(numberOfX(4,A) == 2){
 				cout <<"二舉"<<endl;
 				continue;		
