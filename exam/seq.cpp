9d61558d0a12bf345ced0d4eaa422b8839743410
kami <jingzihao.53@gmail.com>
2016-10-14 20:10:18 -0700

    pre-order sum + modular arithmetic

--- exam/seq.cpp
+++ exam/seq.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 using namespace std;
-int N,Q,M =0;
+int N = 0 ,Q=0,M =0;
 int A[100000];
+int B[100000];
 int main(){
 	cin >> N >> Q >> M;
 	for(int i =1; i<=N;i++){
 		cin >> A[i];
+		A[i] = A[i] % M;
 	}
+	B[0] = 0;
+	for(int i = 1; i <= N; i++){
+		B[i] = (B[i-1]% M +A[i]) % M ; 
+	}
+	
 	int boo = 0;
 	int L,R,V =0;
 	
@@ -15,16 +22,15 @@ int main(){
 		if(boo == 0){
 			cin >>L>>R>>V;
 			for(int i=L;i <=R;i++){
-				A[i] = A[i]+ V;	
-			}		
-		}else{
-			int sum =0;
-			cin >>L>>R;
-			for(int i=L;i <=R;i++){
-				sum = sum + A[i];
+				A[i] = (A[i] + V % M) % M;
 			}
-			sum = sum % M;
-			cout << sum << endl;
+			// dont forgot to use mod
+			for(int i = 1; i <= N; i++){
+				B[i] = (B[i-1] % M  + A[i]) % M;
+			}	
+		}else{
+			cin >> L>> R;		
+			cout <<  (M +(B[R] - B[L-1]) % M) % M << endl;
 		}		
 	}
 	return 0;	
