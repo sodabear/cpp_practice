70f2d372af92ba946277a428b8150f4bbcd363c6
kami <jingzihao.53@gmail.com>
2016-10-15 21:00:29 -0700

    finish fasterpower

--- exam2/healthier.cpp
+++ exam2/healthier.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 int pan[50001];
+int fastpower(int a ,int b, int mod){
+	int ans =1;
+	b = b % mod;
+	while(a > 0){
+		if(a & 1){
+			ans = (ans * b) % mod;
+		}
+		a = a>>1;
+		b = (b * b) % mod;
+		
+		}
+	return ans;			
+}
+
 int main(){
 		int n,d;
 		cin >> n >> d;
@@ -8,10 +22,8 @@ int main(){
 		for(int i = 1; i <= n; i++){
 			cin>>ci>>ki;
 			temp = 1;
-			for(int j =1 ; j <= d; j++){
-				temp = (ki *(temp % 4)) %4;	
-			}
-			pan[i] = (temp* (ci %4)) %4;
+			temp = fastpower(d,ki,4);
+			pan[i] = (temp* (ci %4)) % 4;
 		}	
 		int sum = 0;
 		for(int i = 1; i <= n;i++){
