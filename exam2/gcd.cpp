679c684448772934f84e88dcdd6d7efccdd2425d
kami <jingzihao.53@gmail.com>
2016-10-15 20:35:23 -0700

    pass gcd sample test

--- exam2/gcd.cpp
+++ exam2/gcd.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 using namespace std;
 int a[10000];
+
+int gcd(int a , int b){
+	if (b == 0){
+		return a;
+	}	
+	return gcd(b, a %b);
+}
 int main(){
 	int n;
 	cin >> n;
 	
-	for(int i =1 ; i <= n;i++){
+	for(int i =1; i <= n;i++){
 		cin >> a[i];
 	}	
-	for(int i =1 ; i <= n;i++){
-		//a[i+1] = gcd(a[i],a[i+1]);
+	for(int i =1; i+1 <= n;i++){
+		a[i+1] = gcd(a[i],a[i+1]);
 	}
-	
-	
-	
-	
-	
-	//cout << a[a]
-	cout << 15 << endl;
+	cout << a[n] << endl;
 	return 0;
 }
