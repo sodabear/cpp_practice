1c9301b7ef23968cafd9397642439e691789b4f7
kami <jingzihao.53@gmail.com>
2016-10-15 18:52:56 -0700

    push gcd sample

--- exam2/healthier.cpp
+++ exam2/healthier.cpp
@@ -9,13 +9,13 @@ int main(){
 			cin>>ci>>ki;
 			temp = 1;
 			for(int j =1 ; j <= d; j++){
-				temp = (ki *(temp % 4) %4);	
+				temp = (ki *(temp % 4)) %4;	
 			}
 			pan[i] = (temp* (ci %4)) %4;
 		}	
 		int sum = 0;
 		for(int i = 1; i <= n;i++){
-			sum = (sum + (pan[i] %4) %4);
+			sum = (sum + (pan[i] %4)) %4;
 			
 		}
 		cout << sum <<endl;
