6ad9c1e253d98f0a3db2551586d33b55c30f2b24
kami <jingzihao.53@gmail.com>
2016-10-15 21:55:30 -0700

    finally pass

--- exam2/healthier.cpp
+++ exam2/healthier.cpp
@@ -1,34 +1,43 @@
 #include <iostream>
 using namespace std;
 int pan[50001];
-int fastpower(int a ,int b, int mod){
-	int ans =1;
-	b = b % mod;
-	while(a > 0){
-		if(a & 1){
-			ans = (ans * b) % mod;
-		}
-		a = a>>1;
-		b = (b * b) % mod;
-		
-		}
-	return ans;			
-}
+
 
 int main(){
 		int n,d;
+		// n is number of panzi
 		cin >> n >> d;
-		int ci,ki,temp;
+		int ci,ki;
 		for(int i = 1; i <= n; i++){
+			int temp = 0;
 			cin>>ci>>ki;
-			temp = 1;
-			temp = fastpower(d,ki,4);
-			pan[i] = (temp* (ci %4)) % 4;
-		}	
-		int sum = 0;
+		// if d is even: ki even^even % 4 = 0
+		// if d is even: ki even^odd  % 4 = 2
+		// if d is odd:  ki odd^even  % 4 = 1
+		// if d is odd:  ki odd^odd   % 4 = 3
+			if(ki % 4 == 0){
+				 temp = 0;	
+			}else if(ki % 4 == 1){
+				 temp = 1;
+			}else if(ki % 4 == 2){
+				if(d == 1){
+					temp = 2;
+				}else{
+					temp = 0;
+				}		
+			}else{
+				if(d & 1){
+					temp = 3;
+				}else{
+					temp = 1;
+				}
+				
+			}		
+			pan[i] = ((ci%4) *temp) % 4;
+		}				
+		int sum = 0; 
 		for(int i = 1; i <= n;i++){
-			sum = (sum + (pan[i] %4)) %4;
-			
+			sum = (sum + pan[i]) %4;
 		}
 		cout << sum <<endl;
 		return 0;
@@ -37,4 +46,5 @@ int main(){
 	
 	
 }	
+
 
